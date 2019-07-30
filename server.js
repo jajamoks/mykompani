95cb912a4889d25b63ccd33e373f7e6dd565694c
fortunetede <fortunetedegh@gmail.com>
2019-07-30 12:39:16 +0000

    init push1

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path')
 // create the express app
 const app = express()
 // create middleware to handle the serving the app
-app.use("*", serveStatic(path.join(__dirname, '/dist/')))
+app.use("/", serveStatic(path.join(__dirname, '/dist/')))
 // Catch all routes and redirect to the index file
-// app.get('*', function (req, res) {
-//     res.sendFile(__dirname + '/dist/index.html')
-// })
+app.get('*', function (req, res) {
+    res.sendFile(__dirname + '/dist/index.html')
+})
 // Create default port to serve the app on
 const port = process.env.PORT || 5000
 app.listen(port)
