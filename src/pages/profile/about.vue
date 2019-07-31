bc53fc98e642affd4cf34ba591c2d3e13ec39f0e
fortunetede <fortunetedegh@gmail.com>
2019-07-31 16:10:16 +0000

    make addEducation a modal

--- src/pages/profile/about.vue
+++ src/pages/profile/about.vue
@@ -35,96 +35,7 @@
                 </li>
               </ul>
             </div>
-            <div  class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-              <div class="modal fade" id="educationModal" ref="employeeEducation" tabindex="-1" role="dialog" aria-labelledby="formModal"
-                aria-hidden="true">
-                <div class="modal-dialog" role="document">
-                  <form @submit.prevent="addEducation">
-                    <div class="modal-content">
-                      <div class="modal-header">
-                        <h5 class="modal-title" id="formModal">Add Education</h5>
-                        <button type="button" id="close" class="close" data-dismiss="modal" aria-label="Close">
-                          <span aria-hidden="true">&times;</span>
-                        </button>
-                      </div>
-                      <div class="modal-body">
-                        <label for="password">Collage / Institution</label>
-                        <div class="form-group">
-                          <div class="form-line">
-                            <input type="text" v-validate="'required'" v-model="education.institution" class="form-control"
-                              placeholder="University Of Ghana" name="institution">
-                               <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('institution') }}</span>
-                          </div>
-                        </div>
-                        <label for="password">Country</label>
-                        <div class="form-group">
-                          <div class="form-line">
-                            <select v-validate="'required'" v-model="education.country" class="form-control" name="country">
-                              <option value="Ghana">Ghana</option>
-                              <option value="USA">USA</option>
-                              <option value="Togo">Togo</option>
-                            </select>
-                            <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('country') }}</span>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-8">
-                            <label for="password">Course</label>
-                            <div class="form-group">
-                              <div class="form-line">
-                                <select v-validate="'required'" v-model="education.course" class="form-control" name="course">
-                                  <option value="0">Choose your course</option>
-                                  <option value="Computer Science">Computer Science</option>
-                                  <option value="Accounting">Accounting</option>
-                                  <option value="Law">Law</option>
-                                </select>
-                                <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('course') }}</span>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-md-4">
-                            <label for="password">Cert Type</label>
-                            <div class="form-group">
-                              <div class="form-line">
-                                <select v-validate="'required'" v-model="education.cert_type" class="form-control" name="cert_type">
-                                  <option value="High School">High School</option>
-                                  <option value="Degree">Degree</option>
-                                  <option value="Masters">Masters</option>
-                                </select>
-                                <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('cert_type') }}</span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                        <div class="row">
-                          <div class="col-md-6">
-                            <label for="password">Admission Date</label>
-                            <div class="form-group">
-                              <div class="form-line">
-                                <vue-monthly-picker v-validate="'required'" v-model="education.start_date" name="start_date"> </vue-monthly-picker>
-                                <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('start_date') }}</span>
-                              </div>
-                            </div>
-                          </div>
-                          <div class="col-md-6">
-                            <label for="password">Graduration Date</label>
-                            <div class="form-group">
-                              <div class="form-line">
-                                <vue-monthly-picker v-validate="'required'" v-model="education.end_date" name="end_date"> </vue-monthly-picker>
-                                <span style="margin-top: 0.5em;" class="text-danger">{{ errors.first('end_date') }}</span>
-                              </div>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                      <div class="modal-footer">
-                        <button type="submit" class="btn btn-info waves-effect">Save</button>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </div>
-            </div>
+            <add-education></add-education>
           </div>
         </div>
 
@@ -268,10 +179,12 @@
   } from "vuex";
   import axios from 'axios'
   import endpoints from '../../apis/endpoints'
+  import addEducation from '../../modals/add_education'
   import VueMonthlyPicker from 'vue-monthly-picker'
   export default {
     components: {
-      VueMonthlyPicker
+      VueMonthlyPicker,
+      addEducation
     },
     data: () => ({
       education: {
@@ -309,7 +222,6 @@
           this.$loading.close()
         }
       }
-
     },
     computed: {
       ...mapState({
